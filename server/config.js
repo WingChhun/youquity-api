206af79e41e322b34c8593d42ffad8d566ee6438
Amanda Reilly <amandamichellereilly@gmail.com>
2018-12-05 14:50:38 -0800

    Correct default CLIENT_ORIGIN in config file.

--- server/config.js
+++ server/config.js
@@ -4,4 +4,4 @@ exports.PORT = process.env.PORT || 8080;
 exports.TEST_PORT = process.env.TEST_PORT || 8888;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'The quick brown fox jumped over the lazy dog.';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000/';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
